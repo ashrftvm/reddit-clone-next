33d05edac6272cb6ce66021203b5984f2801fc23
Ashrf <ashrftvm1@gmail.com>
2023-07-23 13:35:41 +0400

    updated ghost btn styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
           "bg-zinc-100 text-zinc-900 hover:bg-zinc-200 outline outline-1 outline-zinc-300",
         subtle: "hover:bg-zinc-200 bg-zinc-100 text-zinc-900",
         ghost:
-          "bg-transparent hover:bg-zinc-100 text-zinc-800 data-[state=open]:bg-transparent data-[state=open]:bg-transparent",
+          "bg-transparent hover:bg-zinc-100 hover:text-zinc-800 text-zinc-200 data-[state=open]:bg-transparent data-[state=open]:bg-transparent",
         link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
       },
       size: {
