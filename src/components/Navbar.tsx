1b4844e440403ebd7bbc264a197d1ec723eabfb9
Ashrf <ashrftvm1@gmail.com>
2023-07-23 13:34:53 +0400

    removed async keyword from navbar return

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 
-const Navbar = async () => {
+const Navbar = () => {
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-800 border-b border-zinc-950 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
